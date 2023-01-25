d04ab9a5b2fef0b964797fe6fbd4fad544501403
qinghengxk <qinghengxk@qq.com>
2023-01-25 20:26:05 +0800

    Improve GUI

--- THXLauncher/src/main/java/com/qinghengxk/thxlauncher/InfoActivity.java
+++
@@ -1,6 +0,0 @@
-package com.qinghengxk.thxlauncher;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-public class InfoActivity extends AppCompatActivity {
-}
---
+++ THXLauncher/src/main/java/com/qinghengxk/thxlauncher/about_info_activity.java
@@ -0,0 +1,14 @@
+package com.qinghengxk.thxlauncher;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class about_info_activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_info);
+    }
+}
